ab612725927f7052397483dd22669c5ad410b33c
fritschcm272 <fritschcm27@gmail.com>
2023-12-19 14:38:15 -0600

    Update UWWMensBball.py

--- UWWMensBball.py
+++ UWWMensBball.py
@@ -390,8 +390,8 @@ st.dataframe(
         "UWW_PLUS_MINUS": "Current Points +/-",
         "UWW_PLUS_MINUS_CUMSUM": st.column_config.LineChartColumn(
             "Trending +/-", 
-            y_min= 0,#min_pm, 
-            y_max= max_pm
+            # y_min= 0,#min_pm, 
+            # y_max= max_pm
         ),
         "UWW_ASST_TUN": "Assist/Turnover",
         "UWW_REBOUNDING": "Rebounding +/-"
